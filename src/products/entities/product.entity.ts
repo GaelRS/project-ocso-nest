32c872224eb15eedfa80d4fd91026b4e8b2fabfb
GaelRS <grosas04@alumnos.uaq.mx>
2025-03-07 15:11:00 -0600

    Relations between entities in the DB

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,5 @@
 import { Provider } from 'src/providers/entities/provider.entity';
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 @Entity()
 export class Product {
     @PrimaryGeneratedColumn('uuid')
@@ -12,5 +12,6 @@ export class Product {
     countSeal: number;
 
     @ManyToOne(() => Provider, provider => provider.products)
+    @JoinColumn({name: 'providerId'})
     provider: Provider;
 }
--- src/regions/entities/region.entity.ts
+++ src/regions/entities/region.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Location } from "src/locations/entities/location.entity";
 
 @Entity()   
 export class Region {
@@ -11,4 +12,7 @@ export class Region {
     regionName: string;
     @Column('simple-array')
     regionStates: string[];
+
+    @OneToMany(() => Location, (location) => location.region)
+    locations: Location[];
 }
