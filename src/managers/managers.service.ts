3f599d38ba4cc61e8a19149636d9774f2fcf2970
GaelRS <grosas04@alumnos.uaq.mx>
2025-04-21 14:52:53 -0600

    feat: add relations in mangerService

--- src/managers/managers.service.ts
+++ src/managers/managers.service.ts
@@ -19,8 +19,12 @@ export class ManagersService {
   }
 
   findOne(id: string) {
-    const manager = this.managerRepository.findOneBy({
-      managerId: id,
+    const manager = this.managerRepository.findOne({
+      where: { managerId: id },
+      relations:{
+        location: true,
+      }
+
     });
     if(!manager) throw new NotFoundException('Manager not found');
     return manager;
